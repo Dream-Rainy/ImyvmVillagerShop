4ce0f097c6d5b6386dc48a15dfac9b849a5399fe
Dream-Rainy <88184993+Dream-Rainy@users.noreply.github.com>
2023-06-03 15:52:34 +0800

    chore：添加了自建仓库，修改了github action文件

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,14 +25,17 @@ repositories {
 	// Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
 	// See https://docs.gradle.org/current/userguide/declaring_repositories.html
 	// for more information about repositories.
-	// Imyvm Hoki
 	maven {
 		name = "Fabric"
 		url = uri("https://maven.fabricmc.net/")
 	}
 
+	// Imyvm Hoki
 	maven {
 		url = uri("https://maven.victorica.dev")
+	}
+	maven {
+		url = uri("https://share.dreamrain.top")
 	}
 
 	maven {
